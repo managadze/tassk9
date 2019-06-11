fd5830294a7f05e3ffaa8d5f383d9db44047a6bc
Алина Манагадзе <agmanagadze_1@edu.hse.ru>
2019-06-11 22:49:20 +0500

    Исправленная

--- tassk9/BiList.cs
+++ tassk9/BiList.cs
@@ -6,19 +6,19 @@ using System.Threading.Tasks;
 
 namespace tassk9
 {
-    class BiList
+    public class Node
     {
         public int data;
-        public BiList next, last;
+        public Node next, last;
 
-        public BiList()
+        public Node()
         {
             data = 0;
             next = null;
             last = null;
         }
 
-        public BiList(int d)
+        public Node(int d)
         {
             data = d;
             next = null;
@@ -28,6 +28,129 @@ namespace tassk9
         public override string ToString()
         {
             return data.ToString() + " ";
+        }  
+    }
+
+    public class BiList
+    {
+        private Node beg;
+        private Node tail;
+
+        public int Count
+        {
+            get; private set; 
+        }
+
+        public Node Add(int el)
+        {
+            if (beg != null)
+            {
+                Node temp = new Node(el);
+                Node t = beg;
+                while (t.next != null) t = t.next;
+                t.next = temp;
+                temp.last = t;
+            }
+            else beg = new Node(el);
+            Count++;
+            return beg;
+        }
+
+        public Node Create(int N, int i)
+        {
+            if (i <= N)
+            {
+                Node head = beg;
+                head = Add(i);
+                i++;
+                head = Create(N, i++);
+                return beg;
+            }
+            else return beg;
+        }
+
+        public void Research(int num, int count)
+        {
+            Node head = beg;
+            this.Researcher(num, count, head);
+        }
+
+        void Researcher(int num, int count, Node head)
+        {
+            if (head != null)
+            {
+                Node temp = head;
+                if (temp.data == num)
+                {
+                    Console.WriteLine($"Элемент {temp.data} найден на {count+1} месте");
+                }
+                else if (temp.next != null)
+                {
+                    count++;
+                    temp = temp.next;
+                    Researcher(num, count, temp);
+                }
+                else if (temp.next == null)
+                {
+                    Console.WriteLine("Элемент не найден");
+                }
+            }
+        }
+
+        public void Delete(int n)
+        {
+            Node head = beg;
+            this.Delete(n, head);
+            Count--;
+        }
+
+        void Delete(int n, Node head)
+        {
+            if (head != null)
+            {
+                Node temp = head;
+                Node k = head;
+                if ((temp.data == n) && (temp.next != null))
+                {
+                    k = temp.next;
+                    k.last = null;
+                    Console.WriteLine("Элемент удален");
+                }
+                else if ((temp.next != null) && (temp.next.next != null) && (temp.next.data == n))
+                {
+                    temp = temp.next = temp.next.next;
+                    Console.WriteLine("Элемент удален");
+                }
+                else if ((temp.next != null) && (temp.next.next == null) && (temp.next.data == n))
+                {
+                    temp.next = null;
+                    Console.WriteLine("Элемент удален");
+                }
+                else
+                {
+                    if (temp.next != null)
+                    {
+                        temp = temp.next;
+                        Delete(n, temp);
+                    }
+                }
+            }
+        }
+
+        public void Show()
+        {
+            if (beg != null)
+            {
+                Node temp = beg;
+                Console.WriteLine(@"Список:");
+                while (temp != null)
+                {
+                    Console.Write(temp + " ");
+                    temp = temp.next;
+                }
+                Console.WriteLine();
+            }
+            else Console.WriteLine("Список пуст");
         }
     }
 }
